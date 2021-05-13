ba040e33734dc816ab7f0f2dba68d3f4ed6e706c
flycrum <41925404+flycrum@users.noreply.github.com>
2021-05-13 09:02:06 -0500

    chore: add chakra emotion framer react-icons and react-virtualized packages

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import store from './store/store';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
+import { ColorModeScript } from '@chakra-ui/react';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
 	<Provider store={store}>
+		<ColorModeScript />
 		<App/>
 	</Provider>,
 	document.getElementById('root'),
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorker.unregister();
