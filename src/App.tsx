385a9b481b56af34bc5969fede4ffd36caf7e1db
flycrum <41925404+flycrum@users.noreply.github.com>
2021-05-26 17:25:45 -0500

    refactor: remove grommet and now redundant styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import { storePersistor } from './store/store';
 import { PersistGate } from 'redux-persist/integration/react';
-import { Box, Heading, Text } from 'grommet';
 import { TeamsList } from './view/components/TeamsList';
 import {
-	GrommetStyled,
-	grommetTheme,
-} from './view/components/GrommetAppThemeStyled';
 
 function App() {
 	return (
@@ -14,29 +10,6 @@ function App() {
 			loading={null}
 			persistor={storePersistor}
 		>
-			<GrommetStyled theme={grommetTheme}>
-				<Box
-					fill
-					overflow="hidden"
-					pad="xsmall"
-				>
-					<Box
-						align="center"
-						style={{ minHeight: 'initial' }}
-					>
-						<Heading level="3">
-							Common Opponents
-						</Heading>
-						<Text>
-							Select teams to find their common opponents...within 2 degrees of separation.
-						</Text>
-					</Box>
-					<Box direction="row-responsive">
-						<TeamsList teamOption={1} />
-						<TeamsList teamOption={2} />
-					</Box>
-				</Box>
-			</GrommetStyled>
 		</PersistGate>
 	);
 }
--- src/view/components/TeamsList.tsx
+++ src/view/components/TeamsList.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Box, Button, Image, Text } from 'grommet';
 import { useAppDispatch, useAppSelector } from '../../store/store';
 import { apiTeamsSelectors } from '../../store/apiTeamsSlice';
-import styled from 'styled-components';
 import { simActions } from '../../store/simSlice';
 import type { ApiTeamsResponseTeamItem } from '../../types/apiTeams';
 
@@ -13,7 +11,6 @@ interface Props {
 /**
  * A select component for the list of all teams.
  */
-export const TeamsList = ({ teamOption }: Props) => {
 	const teams = useAppSelector(apiTeamsSelectors.selectAll);
 	const { team1, team2 } = useAppSelector((state) => state.sim);
 	const dispatch = useAppDispatch();
@@ -27,26 +24,10 @@ export const TeamsList = ({ teamOption }: Props) => {
 		);
 	};
 
-	return (
-		<Box
-			as="ul"
-			pad={{ horizontal: "large" }}
-			width={{ min: '50%' }}
-			overflow="auto"
-			// position scrollbars based on side
-			style={{ direction: teamOption === 1 ? 'rtl' : 'ltr' }}
-		>
-			{teams.map((team, index) => (
 				<Box
-					as="li"
 					key={team.team.id}
-					direction="row"
-					align="center"
-					justify={teamOption === 1 ? 'end' : 'start'}
-					// justify="between"
 					// pad="small"
 					height={{ min: 'auto' }}
-					border={index ? "bottom" : "horizontal"}
 					style={{
 						borderImageSource,
 						borderImageSlice: 1,
@@ -54,66 +35,11 @@ export const TeamsList = ({ teamOption }: Props) => {
 						direction: 'ltr',
 					}}
 				>
-					<ButtonStyled
-						data-selected={teamOption === 1 ? team === team1 : team === team2}
 						onClick={() => setTeam(team)}
 						teamOption={teamOption}
-					>
-						{teamOption === 2 && (
-							<TeamLogo url={team.team.logos[0]?.href} />
-						)}
-						<Text>
-							{ team.team.displayName }
-						</Text>
-						{teamOption === 1 && (
-							<TeamLogo url={team.team.logos[0]?.href} />
-						)}
-					</ButtonStyled>
 				</Box>
-			))}
-		</Box>
 	);
 }
 
-const TeamLogo = ({ url }: { url: string }) => (
-	<Box
-		width="small"
-		// width="xsmall"
-		height="xxsmall"
-		// height="32px"
-	>
-		<Image
-			src={url}
-			fit="cover"
-		/>
-	</Box>
 );
-
-const ButtonStyled = styled(Button).attrs((props: any) => ({
-	teamOption: props.teamOption,
-}))`
-	display: flex;
-	align-items: center;
-	width: 100%;
-	justify-content: ${(props: any) => props.teamOption === 1 ? 'flex-end' : 'flex-start'};
-	filter: grayscale(100%);
-	//transform: scale(1.0);
-	transition: transform 0.5s ease, filter 1.5s ease;
-
-	&[data-selected="true"],
-	&:hover {
-		filter: grayscale(0%);
-		//transform: scale(1.2);s
-	}
-
-	& > div {
-		transition: height 0.3s ease-in-out;
-	}
-
-	&[data-selected="true"] > div {
-		height: 180px;
-		//height: 100px;
-	}
-`;
-
 
